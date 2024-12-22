157d62f7f5f42372084eb69ca52a551573b2c273
SaeedGeshani <saeedgeshani2@gmail.com>
2024-12-22 16:39:13 +0330

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,20 @@
 import msvcrt
 import os
+#*************** SALEH IN DICTIONARY RO BAYAD UPDATE KONI ****************************
+data ={
+    "player1_name": "Saeed",
+    "player2_name": "Saleh",
+    "player1_loc" : [2,10],
+    "player2_loc" : [4,8],
+    "obstacles_loc" : [[1,5],[3,5],[2,9]],
+    "turn" : "player1",
+    "time_spent" : 0,
+    "game_result_for_player1": None,
+    "end_game_date" : None,
+    "Game_ID" : 1234,
+    "turn" : 0
+}
+#**************************************************************************************
 
 players = ["A","B"]
 
